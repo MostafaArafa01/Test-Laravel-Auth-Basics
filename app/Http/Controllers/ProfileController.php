9cb2c74719f2430b5e1966ce3db59a5709d41df2
MostafaArafa01 <mostafaarafa478@gmail.com>
2024-10-10 17:05:13 +0300

    Solve tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,7 +15,16 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
-
+        $user = auth() -> user();
+        $user -> update([
+            'name' => $request -> name,
+            'email' => $request -> email
+        ]);
+        if($request->filled('password')){
+            $user->update([
+                'password' => Hash::make($request -> password)
+            ]);
+        }
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
